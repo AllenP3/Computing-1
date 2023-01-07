106491d72d6a4fc90af507c03ee3505aa5a3f768
AllenP3 <72021357+AllenP3@users.noreply.github.com>
2023-01-07 14:15:43 +0100

    minor

--- hm4.cpp
+++ hm4.cpp
@@ -96,4 +96,58 @@ int main(){
     cout << "The number of permutations of " <<n<<" out of "<<k<<" is "<<Perm(n, k);
 
     
+}
+Problem 3
+
+#include <iostream>
+
+using namespace std;
+
+long fac(int n){
+    if(n < 0)
+        return 0;
+
+    int f = 1;
+    while (n > 1){
+        f = f * n--;
+    }
+    return f;
+}
+
+long Comb(int n, int k){
+    return fac(n) / (fac(k)* fac((n-k)));
+
+}
+int main(){
+    int n, k;
+    cout << "Enter number of elements in set: ";
+    cin >> n;
+    cout << "Enter number of elements to be taken out of the set: ";
+    cin >> k;
+    cout << "The number of combinations of " <<n<<" out of "<<k<<" is "<<Comb(n, k);
+}
+
+Problem 4
+
+#include <iostream>
+
+using namespace std;
+
+double Power(double x, int p){
+    int res = x;
+
+    for(int i = 1; i <= p-1; ++i){
+           res = res * x; 
+    }
+
+    return res;
+}
+
+int main(){
+    float num1;
+    int num2;
+
+    cout << "Enter a floating point number and an integer: ";
+    cin >> num1 >> num2;
+    cout << "Power("<<num1<<", "<<num2<<") = "<<Power(num1, num2);
 }*/
