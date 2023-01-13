a14139d0111e67d7cabc61e700564add9004652e
AllenP3 <72021357+AllenP3@users.noreply.github.com>
2023-01-13 13:51:10 +0100

    minor

--- commandline.cpp
+++ commandline.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
+#include <cstring>
+#include <string>
 
 using namespace std;
 
+int add(int first, int second){
+    return first + second;
+}
+
 int main(int argc, char* argv[]){
-    if(argc != 2){
-        cout << "Usage: hello <your name>"<<endl;
+    if(argc != 4){
+        cout << "Invalid number of input parameters. Returning..."<<endl;
+        return (1);
+    }
+    if(strcmp(argv[2], "+") != 0){
+        cout << "Unexpected operator (not +). Returning..."<<endl;
+        return(-1);
     }
 
+    int a = stoi(argv[1]);
+    int b = stoi(argv[3]);
+    cout << a << " + " << b << " = " << add(a, b) << endl;
+
     cout << "Hello "<< argv[1] << " !" <<endl;
+    
+    return 0;
 }
